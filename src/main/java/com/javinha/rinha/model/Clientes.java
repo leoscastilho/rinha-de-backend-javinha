9d0eca7f018895cdcb60df72e8c0ae4be4681df0
Ettore <ettore.scolar98@gmail.com>
2024-02-10 16:32:54 -0300

    fix id annotation and add getter and setter

--- src/main/java/com/javinha/rinha/model/Clientes.java
+++ src/main/java/com/javinha/rinha/model/Clientes.java
@@ -1,20 +1,44 @@
 package com.javinha.rinha.model;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
+
 
 @Entity
 public class Clientes {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private Long limite;
 
 	public Clientes() {
 		
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getLimite() {
+		return limite;
+	}
+
+	public void setLimite(Long limite) {
+		this.limite = limite;
 	}
 }
