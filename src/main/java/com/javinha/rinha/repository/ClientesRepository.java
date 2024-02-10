87e6cebde2f47b0d7bc80dc7696d9cb4b9dc6981
Ettore <ettore.scolar98@gmail.com>
2024-02-10 16:33:20 -0300

    add repository annotation and created new query

--- src/main/java/com/javinha/rinha/repository/ClientesRepository.java
+++ src/main/java/com/javinha/rinha/repository/ClientesRepository.java
@@ -3,9 +3,12 @@ package com.javinha.rinha.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.javinha.rinha.model.Clientes;
 
+@Repository
 public interface ClientesRepository extends JpaRepository<Clientes,Long>{
-	List<Clientes> findAll();
+	List<Clientes> findAllById(Long id);
+
 }
